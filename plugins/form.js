78439861d6c43d6968158c5ac78a07f4d386900f
bryku <drburnett@mail.com>
2023-05-02 11:25:02 -0700

    Create form.js

---
+++ plugins/form.js
@@ -0,0 +1,12 @@
+j.plugins.form = function(e, v){
+	e.addEventListener('submit',(event)=>{
+		event.preventDefault();
+		let data = {};
+		let form = new FormData(event.target);
+		for (const [key, value] of form) {
+			data[key] = value
+		}
+		v(event, e, data)
+	})
+	return e
+}
